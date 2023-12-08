8833d1a6937febc460dba8a01f635fac5a883a6b
Thalyson Rian <Trungr@hotmail.com>
2023-12-08 02:57:01 -0300

    Criei a página home, além disso, criei uma flatlist para um menu rápido (educação, saúde, medicina, et.c

--- App.tsx
+++ App.tsx
@@ -6,15 +6,18 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Cadastro from './src/screens/Cadastro';
 import Login from './src/screens/Login';
+import Home from './src/screens/Home';
 
 const Stack = createStackNavigator()
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Cadastro" screenOptions={{headerShown: false}}>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
         <Stack.Screen name="Cadastro" component={Cadastro} />
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
